f1ae87bd9df2c3d70db980ea5e721223b545da5f
Ferrix Hovi <ferrix@ferrix.fi>
2012-07-21 20:51:00 +0300

    Add setup.py
    Enable packaging the library.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Djheroku',
+      version='0.1',
+      description='Some helper functionality for binding Heroku configuration to Django',
+      author='Ferrix Hovi',
+      author_email='ferrix+git@ferrix.fi',
+      url='http://github.com/ferrix/djheroku/',
+      packages=['djheroku']
+     )
