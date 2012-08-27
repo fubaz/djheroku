083c75088434eeeceac1f7042bff1924dc9745e8
Ferrix Hovi <ferrix.hovi@f-secure.com>
2012-08-28 01:15:58 +0300

    Add setup dependency to unittest2
    Travis failed because unittest2 was not explicitly named. Also, the
    package now uses setuptools.

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='Djheroku',
-      version='0.1',
+      version='0.2',
       description='Some helper functionality for binding Heroku configuration to Django',
       author='Ferrix Hovi',
       author_email='ferrix+git@ferrix.fi',
       url='http://github.com/ferrix/djheroku/',
-      packages=['djheroku']
+      packages=['djheroku'],
+      setup_requires=['unittest2'],
      )
