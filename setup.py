fcf13ba0a74397c45f620bc63d80c5eaf18163dc
Ferrix Hovi <ferrix@ferrix.fi>
2012-10-23 22:11:43 +0300

    Get minor version from environment

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 ''' Setuptools installation script for Djheroku '''
 from __future__ import with_statement
+import os
 
 from setuptools import setup
 
@@ -12,8 +13,15 @@ setup_requirements = ''
 with open('requirements-test.txt') as reqset:
     setup_requirements = reqset.read()
 
+version = '0.3'
+
+minor_version = os.environ.get('DJHEROKU_MINOR_VERSION', None)
+
+if minor_version:
+    version = version + '.' + minor_version
+
 setup(name='Djheroku',
-      version='0.3',
+      version=version,
       description='Some helper functionality for binding Heroku configuration to Django',
       author='Ferrix Hovi',
       author_email='ferrix+git@ferrix.fi',
