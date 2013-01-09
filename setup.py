edfe027d3bc1b18de98a46883558e7c0fe486cc2
Ferrix Hovi <ferrix@ferrix.fi>
2013-01-09 20:56:42 +0200

    Fix amateur mistake with version when no Git present

--- setup.py
+++ setup.py
@@ -16,6 +16,11 @@ with open('requirements-test.txt') as reqset:
 if os.path.exists('.git'):
     import commands
     _, __version__ = commands.getstatusoutput('git describe --tags')
+    with file('version.txt', 'wb') as verfile:
+        verfile.write(__version__)
+elif os.path.exists('version.txt'):
+    with file('version.txt', 'rb') as verfile:
+        __version__ = verfile.readlines()[0].strip()
 else:
     __version__ = 'unknown'
 
@@ -28,4 +33,4 @@ setup(name='Djheroku',
       packages=['djheroku'],
       install_requires=requirements,
       setup_requires=setup_requirements,
-      )
+)
